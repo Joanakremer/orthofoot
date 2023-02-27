b1deb0ee3724ca72218c43cf416420269d654756
Alexandre deucher <alexandredeucher@gmail.com>
2023-02-27 19:50:27 -0300

    Dao agenda para finalizar

---
+++ src/controle/CDaoAgenda.java
@@ -0,0 +1,118 @@
+package controle;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Time;
+import java.util.ArrayList;
+
+import modelo.MAgenda;
+import modelo.MEndereco;
+
+public class CDaoAgenda {
+	private CConexao con;
+	//Insert -------------------------------------------------------------------------------
+		public boolean inserir(MAgenda s) {
+	con = CConexao.getInstancia();
+			Connection c = con.conectar();
+			try {
+				String query = "INSERT INTO agenda (idConsulta, nomeCompleto, contato, data, hora, titulo) VALLUES (?, ?);";
+				PreparedStatement stm = c.prepareStatement(query);
+				
+				stm.setInt(1, s.getIdConsulta());
+				stm.setString(2, s.getNomeCompleto());
+				stm.setString(3, s.getContato());
+				stm.setDate(4, s.getData());
+				stm.setTime(5, s.getHora());
+				stm.setString(6, s.getTitulo());
+				stm.executeUpdate();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			con.fecharConexao();
+			
+			return false;
+		}
+		//Update -------------------------------------------------------------------------------
+		public boolean update(MAgenda s) {
+			con = CConexao.getInstancia();
+					Connection c = con.conectar();
+					try {
+						String query = "UPDATE agenda Set idConsulta = ?, set nomeCompleto = ?, set contato = ?, set data = ?, set hora = ?, set titulo = ? WHERE cep = ?";
+						PreparedStatement stm = c.prepareStatement(query);
+						
+						stm.setInt(1, s.getIdConsulta());
+						stm.setString(2, s.getNomeCompleto());
+						stm.setString(3, s.getContato());
+						stm.setDate(4, s.getData());	
+						stm.setTime(4, s.getHora());			
+						stm.setString(4, s.getTitulo());			
+						stm.executeUpdate();
+						return true;
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					con.fecharConexao();
+					return false;
+				}
+		//Delete -------------------------------------------------------------------------------
+		public boolean delete(MAgenda s) {
+			con = CConexao.getInstancia();
+					Connection c = con.conectar();
+					try {
+						String query = "DELETE FROM agenda Where idConsulta = ?"; 
+						PreparedStatement stm = c.prepareStatement(query);
+						stm.setInt(1, s.getIdConsulta());
+						stm.setString(2, s.getNomeCompleto());
+						stm.setString(3, s.getContato());
+						stm.setDate(4, s.getData());	
+						stm.setTime(4, s.getHora());			
+						stm.setString(4, s.getTitulo());			
+						stm.executeUpdate();
+						return true;
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					con.fecharConexao();
+					return false;
+				}
+		//List -------------------------------------------------------------------------------
+		public ArrayList<MAgenda> listarAgenda() {
+		    ArrayList<MAgenda> agenda = new ArrayList<>();
+		    
+		    con = CConexao.getInstancia();
+		    Connection c = con.conectar();
+		    
+		    try {
+		        Statement stm = c.createStatement();
+		        String query = "SELECT * FROM agenda";
+		        ResultSet rs = stm.executeQuery(query);
+		        while (rs.next()) {
+		            int idConsulta = rs.getInt("idConsulta");
+		            String nomeCompleto = rs.getString("nomeCompleto");
+		            String contato = rs.getString("contato");
+		            Date data = rs.getDate("data");
+		            Time hora = rs.getTime("hora");
+		            String titulo = rs.getString("titulo");
+		            MAgenda s = new MAgenda();
+		            s.setIdConsulta(idConsulta);
+		            s.setNomeCompleto(nomeCompleto);
+		            s.setContato(contato);
+		            s.setData(data);
+		            s.setHora(hora);
+		            s.setTitulo(titulo);
+		            agenda.add(s);
+		        }
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+
+		    con.fecharConexao();
+
+		    return agenda;
+		}
+}
--- src/controle/CDaoEndereco.java
+++ src/controle/CDaoEndereco.java
@@ -1,5 +1,106 @@
 package controle;
 
-public class CDaoEndereco {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import modelo.MEndereco;
+import modelo.MUsuario;
+
+public class CDaoEndereco {
+	private CConexao con;
+	//Insert -------------------------------------------------------------------------------
+		public boolean inserir(MEndereco a) {
+	con = CConexao.getInstancia();
+			Connection c = con.conectar();
+			try {
+				String query = "INSERT INTO endereco (cep, rua, cidade, estado) VALLUES (?, ?);";
+				PreparedStatement stm = c.prepareStatement(query);
+				
+				stm.setInt(1, a.getCep());
+				stm.setString(2, a.getRua());
+				stm.setString(3, a.getCidade());
+				stm.setString(4, a.getEstado());				
+				stm.executeUpdate();
+				return true;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			con.fecharConexao();
+			
+			return false;
+		}
+		//Update -------------------------------------------------------------------------------
+		public boolean update(MEndereco a) {
+			con = CConexao.getInstancia();
+					Connection c = con.conectar();
+					try {
+						String query = "UPDATE endereco Set cep = ?, set rua = ?, set cidade = ?, set estado = ? WHERE cep = ?";
+						PreparedStatement stm = c.prepareStatement(query);
+						
+						stm.setInt(1, a.getCep());
+						stm.setString(2, a.getRua());
+						stm.setString(3, a.getCidade());
+						stm.setString(4, a.getEstado());				
+						stm.executeUpdate();
+						return true;
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					con.fecharConexao();
+					return false;
+				}
+		//Delete -------------------------------------------------------------------------------
+		public boolean delete(MEndereco a) {
+			con = CConexao.getInstancia();
+					Connection c = con.conectar();
+					try {
+						String query = "DELETE FROM endereco Where cep = ?"; 
+						PreparedStatement stm = c.prepareStatement(query);
+						stm.setInt(1, a.getCep());
+						stm.setString(2, a.getRua());
+						stm.setString(3, a.getCidade());
+						stm.setString(4, a.getEstado());				
+						stm.executeUpdate();
+						return true;
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					con.fecharConexao();
+					return false;
+				}
+		//List -------------------------------------------------------------------------------
+		public ArrayList<MEndereco> listarAgenda() {
+			ArrayList<MEndereco> endereco = new ArrayList<>();
+			
+			con = CConexao.getInstancia();
+			Connection c = con.conectar();
+			
+			try {
+				Statement stm = c.createStatement();
+				String query = "SELECT * FROM endereco";
+				ResultSet rs = stm.executeQuery(query);
+				while (rs.next()) {
+					int cep = rs.getInt("cep");
+					String rua = rs.getString("rua");
+					String cidade = rs.getString("cidade");
+					String estado = rs.getString("estado");
+					MEndereco a = new MEndereco();
+					a.setCep(cep);
+					a.setRua(rua);
+					a.setCidade(cidade);
+					a.setEstado(estado);
+					endereco.add(a);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+			con.fecharConexao();
+
+			return endereco;
+		}
 }
